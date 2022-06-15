1114f337b75f07dc986b59c776844b32d9f460ed
Katia <katiaubedam@gmail.com>
2022-06-15 21:20:38 +0200

    Cambios

--- ej04.js
+++ ej04.js
@@ -1,5 +1,5 @@
 let num1 = parseInt(prompt("Escribe un número"))
-let num2 = parseInt(prompt("Escribe un número"))
+let num2 = parseInt(prompt("Escribe otro número"))
 
 let num3 = sumar(num1, num2)
 
--- ej05.js
+++ ej05.js
@@ -1,5 +1,7 @@
 let num1 = parseInt(prompt("Escribe un número"))
-let num2 = parseInt(prompt("Escribe un número"))
+let num2 = parseInt(prompt("Escribe otro número"))
+
+esMayor(num1, num2) ? console.log("El primer número es más grande") : console.log("El primer número no es más grande")
 
 function esMayor(num1, num2) {
     if (num1 > num2) {
@@ -8,5 +10,3 @@ function esMayor(num1, num2) {
         return false
     }
 }
-
-esMayor(num1, num2) ? console.log("El primer número es más grande") : console.log("El primer número no es más grande")
