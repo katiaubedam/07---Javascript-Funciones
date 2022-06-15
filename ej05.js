59e0494b3ce14403d7540a440722ebcb198dcc7a
Katia <katiaubedam@gmail.com>
2022-06-15 21:26:36 +0200

    Cambios

--- ej05.js
+++ ej05.js
@@ -4,9 +4,5 @@ let num2 = parseInt(prompt("Escribe otro número"))
 esMayor(num1, num2) ? console.log("El primer número es más grande") : console.log("El primer número no es más grande")
 
 function esMayor(num1, num2) {
-    if (num1 > num2) {
-        return true
-    } else {
-        return false
-    }
+     return num1 > num2 ? true : false
 }
